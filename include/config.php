da40f888c552aa31c97cab03bedeccfad7b8d3b2
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2022-06-23 12:23:45 +0200

    make it better

--- include/config.php
+++ include/config.php
@@ -18,17 +18,16 @@ $server_root = str_replace ('config/config.php' ,'', $this_file);
 
 define ('web_root' , $web_root);
 define('server_root' , $server_root);
-?>
-<?php
+
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["server"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_db = substr($cleardb_url["web_root"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
